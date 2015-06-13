cc1c3a951f11f2ead814a31e06644c22c6d5acf6
Jae Hoon Jo <whwo0312@gmail.com>
2015-06-13 22:08:32 +0900

    test
    test

--- GameFramework/Boss.cpp
+++ GameFramework/Boss.cpp
@@ -5,7 +5,7 @@ Boss::Boss()
 	mRoot = Root::getSingletonPtr(); 
 	mSceneMgr = mRoot->getSceneManager("main");
 
-	mBossEntity = mSceneMgr->createEntity("Boss", "BossMesh.mesh");
+	mBossEntity = mSceneMgr->createEntity("Boss", "Bossmon.mesh");
 	mBossSceneNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("Boss", Vector3(0.0f, -3150.0f, -5000.0f));
 	mBossSceneNode->attachObject(mBossEntity);
 	mBossSceneNode->scale(0.3f,0.3f,0.3f);
